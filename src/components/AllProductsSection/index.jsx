1fc03195209f4fd1e559558e73042df1e3dda67c
Rohit Kumar <rohitku101223@gmail.com>
2025-04-10 00:32:38 +0530

    sort button added

--- src/components/AllProductsSection/index.jsx
+++ src/components/AllProductsSection/index.jsx
@@ -2,13 +2,27 @@ import { useEffect, useState } from "react";
 import ProductCard from "../ProductCard";
 import Cookies from "js-cookie";
 import "./index.css";
+import ProductsHeader from "../ProductsHeader";
+
+const sortbyOptions = [
+  {
+    optionId: 'PRICE_HIGH',
+    displayText: 'Price (High-Low)',
+  },
+  {
+    optionId: 'PRICE_LOW',
+    displayText: 'Price (Low-High)',
+  },
+]
 
 const AllProductsSection = () => {
   const [productsList, setProductsList] = useState([]);
+  const [activeOptionId, setActiveOptionId] = useState(sortbyOptions[0].optionId);
 
   useEffect(() => {
     const getProducts = async () => {
       const apiUrl = "https://apis.ccbp.in/products";
+      // const apiUrl = "https://apis.ccbp.in/products?sort_by=${activeOptionId}`";
       const jwtToken = Cookies.get("jwt_token");
       const options = {
         headers: {
@@ -37,11 +51,20 @@ const AllProductsSection = () => {
       }
     };
     getProducts();
-  }, []);
+  }, [activeOptionId]);
+
+const updateActiveOptionId = optionId =>{
+  setActiveOptionId(optionId);
+}
 
   return (
-    <div>
-      <h1 className="products-list-heading">All Products</h1>
+    <div className="productContainer">
+      {/* <h1 className="products-list-heading">All Products</h1> */}
+      <ProductsHeader 
+        activeOptionId={activeOptionId}
+        sortbyOptions={sortbyOptions}
+        updateActiveOptionId={updateActiveOptionId}
+      />
       <ul className="products-list">
         {productsList.map((product) => (
           <ProductCard productData={product} key={product.id} />
